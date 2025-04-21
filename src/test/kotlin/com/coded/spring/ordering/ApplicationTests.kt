af938cbb399dedc087c8421c7fff732c8c11a43c
najlaasz <najlaa.alzabi@gmail.com>
2025-04-21 19:28:47 +0300

    authentication

--- src/test/kotlin/com/coded/spring/ordering/ApplicationTests.kt
+++ src/test/kotlin/com/coded/spring/ordering/ApplicationTests.kt
@@ -2,12 +2,18 @@ package com.coded.spring.ordering
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+
+	lateinit var restTemplate : RestTemplate
 
 	@Test
 	fun contextLoads() {
+
+
 	}
 
 }
